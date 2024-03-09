ee455f2c391675402549e91b8b1b337fdc34c413
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-03-09 20:29:55 +0000

    Refactor code so line length is 80 characters

--- script.js
+++ script.js
@@ -19,8 +19,15 @@ function getComputerChoice() {
 function getPlayerChoice() {
     const playerSelection = prompt("Your turn");
 
-    if (playerSelection === null || playerSelection.trim() === "" || !accepted_choices.includes(playerSelection.toUpperCase())) {
-        console.error(`Allowed options: ${accepted_choices.join(", ")}. Try again!`);
+    // This check ensures that we have the required input before continuing any
+    // further with execution.
+    if (
+        playerSelection === null ||
+        playerSelection.trim() === "" ||
+        !accepted_choices.includes(playerSelection.toUpperCase())) {
+        console.error(
+            `Allowed options: ${accepted_choices.join(", ")}. Try again!`
+        );
         return null;
     }
 
@@ -28,7 +35,7 @@ function getPlayerChoice() {
 }
 
 /**
- * 
+ * Simulates a single round of the game.
  * @param {string} playerSelection The player's selection.
  * @param {string} computerSelection The computer's generated option.
  * @returns The player who wins the round. "computer", "player", or "tie".
@@ -56,7 +63,8 @@ function announceWinner(playerScore, computerScore) {
     if (playerScore === computerScore)
         return "No one won. It's a tie";
 
-    return (playerScore > computerScore) ? "You won the challenge" : "You lost the challenge";
+    return (playerScore > computerScore) ?
+        "You won the challenge" : "You lost the challenge";
 }
 
 /**
@@ -78,11 +86,15 @@ function playGame() {
 
         const result = playRound(playerSelection, computerSelection);
         if (result === "computer") {
-            console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+            console.log(
+                `You Lose! ${computerSelection} beats ${playerSelection}`
+            );
             computerScore++;
         }
         else if (result === "player") {
-            console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+            console.log(
+                `You Win! ${playerSelection} beats ${computerSelection}`
+            );
             playerScore++;
         }
         else {
