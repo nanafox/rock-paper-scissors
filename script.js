0665dc31a2f40ad4fae419db809267876683df9f
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-03-09 16:24:29 +0000

    Add Rock, Paper, Scissors implementation

---
+++ script.js
@@ -0,0 +1,104 @@
+const accepted_choices = ['ROCK', 'PAPER', 'SCISSORS'];
+
+/**
+ * Generates a random option for the computer.
+ *
+ * @returns A random option for the computer
+ */
+function getComputerChoice() {
+    return accepted_choices[
+        (Math.floor(Math.random() * accepted_choices.length))
+    ];
+}
+
+/**
+ * Receives the player's option.
+ *
+ * @returns The player's option.
+ */
+function getPlayerChoice() {
+    const playerSelection = prompt("Your turn");
+
+    if (playerSelection === null || playerSelection.trim() === "" || !accepted_choices.includes(playerSelection.toUpperCase())) {
+        console.error(`Allowed options: ${accepted_choices.join(", ")}. Try again!`);
+        return null;
+    }
+
+    return playerSelection.trim().toUpperCase();
+}
+
+/**
+ * 
+ * @param {string} playerSelection The player's selection.
+ * @param {string} computerSelection The computer's generated option.
+ * @returns The player who wins the round. "computer", "player", or "tie".
+ */
+function playRound(playerSelection, computerSelection) {
+    if (computerSelection === playerSelection) { return "tie"; }
+
+    if (computerSelection === 'ROCK' && playerSelection === 'SCISSORS') {
+        return "computer";
+    }
+
+    if (computerSelection === 'PAPER' && playerSelection === 'ROCK') {
+        return "computer";
+    }
+
+    if (computerSelection === 'SCISSORS' && playerSelection === 'PAPER') {
+        return "computer";
+    }
+
+    // the player won the round
+    return "player";
+}
+
+function announceWinner(playerScore, computerScore) {
+    if (playerScore === computerScore)
+        return "No one won. It's a tie";
+
+    return (playerScore > computerScore) ? "You won the challenge" : "You lost the challenge";
+}
+
+/**
+ * Simulates the rock, paper, scissors game.
+ * @returns 0 on success, 1 otherwise.
+ */
+function playGame() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 1; i <= 5; i++) {
+        const computerSelection = getComputerChoice();
+        const playerSelection = getPlayerChoice();
+
+        if (playerSelection === null) {
+            console.error("Invalid player option... Can't continue");
+            return 1;
+        }
+
+        const result = playRound(playerSelection, computerSelection);
+        if (result === "computer") {
+            console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+            computerScore++;
+        }
+        else if (result === "player") {
+            console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+            playerScore++;
+        }
+        else {
+            console.log("It's a tie");
+        }
+
+        console.log(
+            `Round ${i} scores
+        Computer: ${computerScore}.
+        You: ${playerScore}.`
+        );
+    }
+
+    console.log(announceWinner(playerScore, computerScore));
+
+    return 0;
+}
+
+playGame();
