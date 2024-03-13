0b6c7b70f5286e240f10edb0a3dee6e00c14c8fb
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-03-13 15:24:11 +0000

    Refactored code to be semistandard compliant

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const accepted_choices = ['ROCK', 'PAPER', 'SCISSORS'];
+const acceptedChoices = ['ROCK', 'PAPER', 'SCISSORS'];
 
 /**
  * Generates a random option for the computer.
  *
  * @returns A random option for the computer
  */
-function getComputerChoice() {
-    return accepted_choices[
-        (Math.floor(Math.random() * accepted_choices.length))
-    ];
+function getComputerChoice () {
+  return acceptedChoices[
+    (Math.floor(Math.random() * acceptedChoices.length))
+  ];
 }
 
 /**
@@ -16,22 +16,22 @@ function getComputerChoice() {
  *
  * @returns The player's option.
  */
-function getPlayerChoice() {
-    const playerSelection = prompt("Your turn: rock, paper, scissors");
+function getPlayerChoice () {
+  const playerSelection = prompt('Your turn: rock, paper, scissors');
 
-    // This check ensures that we have the required input before continuing any
-    // further with execution.
-    if (
-        playerSelection === null ||
-        playerSelection.trim() === "" ||
-        !accepted_choices.includes(playerSelection.toUpperCase())) {
-        console.error(
-            `Allowed options: ${accepted_choices.join(", ")}. Try again!`
-        );
-        return null;
-    }
+  // This check ensures that we have the required input before continuing any
+  // further with execution.
+  if (
+    playerSelection === null ||
+    playerSelection.trim() === '' ||
+    !acceptedChoices.includes(playerSelection.toUpperCase())) {
+    console.error(
+      `Allowed options: ${acceptedChoices.join(', ')}. Try again!`
+    );
+    return null;
+  }
 
-    return playerSelection.trim().toUpperCase();
+  return playerSelection.trim().toUpperCase();
 }
 
 /**
@@ -40,77 +40,75 @@ function getPlayerChoice() {
  * @param {string} computerSelection The computer's generated option.
  * @returns The player who wins the round. "computer", "player", or "tie".
  */
-function playRound(playerSelection, computerSelection) {
-    if (computerSelection === playerSelection) { return "tie"; }
+function playRound (playerSelection, computerSelection) {
+  if (computerSelection === playerSelection) { return 'tie'; }
 
-    if (computerSelection === 'ROCK' && playerSelection === 'SCISSORS') {
-        return "computer";
-    }
+  if (computerSelection === 'ROCK' && playerSelection === 'SCISSORS') {
+    return 'computer';
+  }
 
-    if (computerSelection === 'PAPER' && playerSelection === 'ROCK') {
-        return "computer";
-    }
+  if (computerSelection === 'PAPER' && playerSelection === 'ROCK') {
+    return 'computer';
+  }
 
-    if (computerSelection === 'SCISSORS' && playerSelection === 'PAPER') {
-        return "computer";
-    }
+  if (computerSelection === 'SCISSORS' && playerSelection === 'PAPER') {
+    return 'computer';
+  }
 
-    // the player won the round
-    return "player";
+  // the player won the round
+  return 'player';
 }
 
-function announceWinner(playerScore, computerScore) {
-    if (playerScore === computerScore)
-        return "No one won. It's a tie";
+function announceWinner (playerScore, computerScore) {
+  if (playerScore === computerScore) { return "No one won. It's a tie"; }
 
-    return (playerScore > computerScore) ?
-        "You won the challenge" : "You lost the challenge";
+  return (playerScore > computerScore)
+    ? 'You won the challenge'
+    : 'You lost the challenge';
 }
 
 /**
  * Simulates the rock, paper, scissors game.
  * @returns 0 on success, 1 otherwise.
  */
-function playGame() {
-    let playerScore = 0;
-    let computerScore = 0;
+function playGame () {
+  let playerScore = 0;
+  let computerScore = 0;
 
-    for (let i = 1; i <= 5; i++) {
-        const computerSelection = getComputerChoice();
-        const playerSelection = getPlayerChoice();
+  for (let i = 1; i <= 5; i++) {
+    const computerSelection = getComputerChoice();
+    const playerSelection = getPlayerChoice();
 
-        if (playerSelection === null) {
-            console.error("Invalid player option... Can't continue");
-            return 1;
-        }
-
-        const result = playRound(playerSelection, computerSelection);
-        if (result === "computer") {
-            console.log(
-                `You Lose! ${computerSelection} beats ${playerSelection}`
-            );
-            computerScore++;
-        }
-        else if (result === "player") {
-            console.log(
-                `You Win! ${playerSelection} beats ${computerSelection}`
-            );
-            playerScore++;
-        }
-        else {
-            console.log("It's a tie");
-        }
-
-        console.log(
-            `Round ${i} scores
-        Computer: ${computerScore}.
-        You: ${playerScore}.`
-        );
+    if (playerSelection === null) {
+      console.error("Invalid player option... Can't continue");
+      return 1;
     }
 
-    console.log(announceWinner(playerScore, computerScore));
+    const result = playRound(playerSelection, computerSelection);
+    if (result === 'computer') {
+      console.log(
+        `You Lose! ${computerSelection} beats ${playerSelection}`
+      );
+      computerScore++;
+    } else if (result === 'player') {
+      console.log(
+        `You Win! ${playerSelection} beats ${computerSelection}`
+      );
+      playerScore++;
+    } else {
+      console.log("It's a tie");
+    }
 
-    return 0;
+    console.log(
+      `Round ${i} scores
+        Computer: ${computerScore}.
+        You: ${playerScore}.`
+    );
+  }
+
+  console.log(announceWinner(playerScore, computerScore));
+
+  return 0;
 }
 
 playGame();
